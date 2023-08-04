7e7b2ae5f48462134a15ba86b1b299c68714966a
Dmitry Kuksa <dmitry.kuksa.kdv@gmail.com>
2023-08-04 14:28:06 +0200

    cors test

--- src/main/java/com/itrailmpool/itrailmpoolviewer/config/CorsConfig.java
+++ src/main/java/com/itrailmpool/itrailmpoolviewer/config/CorsConfig.java
@@ -1,5 +1,7 @@
 package com.itrailmpool.itrailmpoolviewer.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,14 +12,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @PropertySource(value = "classpath:${env:dev}/application.properties")
 @Configuration
 public class CorsConfig {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CorsConfig.class);
 
     @Bean
     public WebMvcConfigurer corsConfigurer(@Value("${cors.allowed.origin}") String allowedOrigin) {
+        LOGGER.info("Allowed origin {}", allowedOrigin);
+
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins(allowedOrigin)
+                        .allowedOrigins("*")
                         .allowedMethods("GET")
                         .allowedHeaders("*")
                         .allowCredentials(true);
